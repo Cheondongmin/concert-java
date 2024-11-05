9477fc37859268914253896278a5705bebb4b26e
dongminCheon <aehdals9900@gmail.com>
2024-11-05 20:47:29 +0900

    fix: redis 동시성 제외

--- src/main/java/com/hhplus/concert/core/domain/payment/PaymentService.java
+++ src/main/java/com/hhplus/concert/core/domain/payment/PaymentService.java
@@ -10,9 +10,7 @@ import com.hhplus.concert.core.domain.user.UserRepository;
 import com.hhplus.concert.core.domain.user.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -24,48 +22,6 @@ public class PaymentService {
     private final QueueRepository queueRepository;
     private final ConcertSeatRepository concertSeatRepository;
     private final PaymentHistoryRepository paymentHistoryRepository;
-    private final PlatformTransactionManager transactionManager;
-    private final PaymentRedisRock paymentRedisRock;
-
-    public PaymentConcertResult paymentConcertForRedis(String token, long reservationId) {
-        String lockKey = "lock:결제:" + token;
-        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        return paymentRedisRock.executeWithLock(
-                lockKey,
-                10,
-                15,
-                () -> transactionTemplate.execute(status -> {
-                    try {
-                        long userId = Users.extractUserIdFromJwt(token);
-                        Users user = userRepository.findById(userId);
-
-                        Queue queue = queueRepository.findByToken(token);
-                        queue.tokenReserveCheck();
-
-                        Reservation reservation = reservationRepository.findById(reservationId);
-                        user.checkConcertAmount(reservation.getSeatAmount());
-
-                        // 낙관적 락을 사용하여 좌석 조회 및 예약 처리
-                        ConcertSeat concertSeat = concertSeatRepository.findById(reservation.getSeatId());
-                        concertSeat.finishSeatReserve();
-
-                        queue.finishQueue();
-                        reservation.finishReserve();
-
-                        Payment payment = paymentRepository.findByReservationId(reservation.getId());
-                        payment.finishPayment();
-
-                        PaymentHistory paymentHistory = PaymentHistory.enterPaymentHistory(userId, payment.getPrice(), PaymentType.PAYMENT, payment.getId());
-                        paymentHistoryRepository.save(paymentHistory);
-
-                        return new PaymentConcertResult(concertSeat.getAmount(), reservation.getStatus(), queue.getStatus());
-                    } catch (Exception e) {
-                        status.setRollbackOnly();
-                        throw e;
-                    }
-                })
-        );
-    }
 
     @Transactional
     public PaymentConcertResult paymentConcert(String token, long reservationId) {
--- src/main/java/com/hhplus/concert/core/domain/reservation/ReservationService.java
+++ src/main/java/com/hhplus/concert/core/domain/reservation/ReservationService.java
@@ -11,8 +11,6 @@ import com.hhplus.concert.core.domain.user.Users;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -25,44 +23,6 @@ public class ReservationService {
     private final UserRepository userRepository;
     private final ReservationRepository reservationRepository;
     private final PaymentRepository paymentRepository;
-    private final PlatformTransactionManager transactionManager;
-    private final ReservationRedisRock reservationRedisRock;
-
-    public ReserveConcertResult reserveConcertForRedis(String token, long scheduleId, long seatId) {
-        String lockKey = "lock:결제:" + token;
-        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        return reservationRedisRock.executeWithLock(
-                lockKey,
-                10,  // waitTime
-                15,  // leaseTime
-                () -> transactionTemplate.execute(status -> {
-                    try {
-                        long userId = Users.extractUserIdFromJwt(token);
-                        Users user = userRepository.findById(userId);
-                        Queue queue = queueRepository.findByToken(token);
-                        queue.tokenReserveCheck();
-                        ConcertSchedule concertSchedule = concertScheduleRepository.findById(scheduleId);
-                        concertSchedule.isSoldOutCheck();
-                        ConcertSeat concertSeat = concertSeatRepository.findById(seatId);
-                        concertSeat.isReserveCheck();
-                        Concert concert = concertRepository.findById(concertSchedule.getConcertId());
-                        Reservation reservation = Reservation.enterReservation(user, concert, concertSeat, concertSchedule);
-                        reservationRepository.save(reservation);
-                        Payment payment = Payment.enterPayment(userId, reservation.getId(), concertSeat.getAmount(), PaymentStatus.PROGRESS);
-                        paymentRepository.save(payment);
-                        return new ReserveConcertResult(
-                                reservation.getId(),
-                                reservation.getStatus(),
-                                reservation.getReservedDt(),
-                                reservation.getReservedUntilDt()
-                        );
-                    } catch (Exception e) {
-                        status.setRollbackOnly();
-                        throw e;
-                    }
-                })
-        );
-    }
 
     @Transactional
     public ReserveConcertResult reserveConcert(String token, long scheduleId, long seatId) {
